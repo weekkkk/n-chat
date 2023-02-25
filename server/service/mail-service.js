8981e6512be0056c4d43831e589a5cc3017e1d5c
weekkkk <nikitanedelko9@gmail.com>
2023-02-25 16:55:20 +0200

    remove env

--- server/service/mail-service.js
+++ server/service/mail-service.js
@@ -1,4 +1,14 @@
+const nodemailer = require('nodemailer');
 class MailService {
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      host: '',
+      port: '',
+      secure: false,
+      auth: {},
+    });
+  }
+
   async sendActivationLink(to, link) {}
 }
 
