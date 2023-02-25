66cab202429e9c40d5c60f669a86e0ad34028f2a
weekkkk <nikitanedelko9@gmail.com>
2023-02-25 19:45:02 +0200

    refresh function

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -36,6 +36,7 @@ class UserController {
       next(e);
     }
   }
+
   async logout(req, res, next) {
     try {
       const { refreshToken } = req.cookies;
@@ -59,6 +60,13 @@ class UserController {
 
   async refresh(req, res, next) {
     try {
+      const { refreshToken } = req.cookies;
+      const userData = await userService.refresh(refreshToken);
+      res.cookie('refreshToken', userData.refreshToken, {
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+      });
+      return res.json(userData);
     } catch (e) {
       next(e);
     }
--- server/service/user-service.js
+++ server/service/user-service.js
@@ -5,6 +5,7 @@ const mailService = require('./mail-service');
 const tokenService = require('./token-service');
 const UserDto = require('../dtos/user-dto');
 const ApiError = require('../exceptions/api-error');
+const userModel = require('../models/user-model');
 
 class UserService {
   async registation(email, password) {
@@ -65,6 +66,29 @@ class UserService {
   async logout(refreshToken) {
     const token = await tokenService.removeToken(refreshToken);
     return token;
+  }
+
+  async refresh(refreshToken) {
+    if (!refreshToken) {
+      throw ApiError.UnauthorizedError();
+    }
+
+    const userData = tokenService.validateRefreshToken(refreshToken);
+    const tokenFromDb = await tokenService.findToken(refreshToken);
+
+    if (!userData || !tokenFromDb) {
+      throw ApiError.UnauthorizedError();
+    }
+
+    const user = userModel.findById(userData.id);
+    const userDto = new UserDto(user);
+    const tokens = tokenService.generateTokens({ ...userDto });
+    await tokenService.saveToken(userDto.id, tokens.refreshToken);
+
+    return {
+      ...tokens,
+      user: userDto,
+    };
   }
 }
 
