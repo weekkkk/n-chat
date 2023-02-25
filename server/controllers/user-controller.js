d12630d52bb0dc79b45c6e34ba6e90f579ea0a52
weekkkk <nikitanedelko9@gmail.com>
2023-02-25 19:32:48 +0200

    logout function

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,8 +1,16 @@
 const userService = require('../service/user-service');
+const { validationResult } = require('express-validator');
+const ApiError = require('../exceptions/api-error');
 
 class UserController {
   async registration(req, res, next) {
     try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return next(
+          ApiError.BadRequest('Ошибка при валидации', errors.array())
+        );
+      }
       const { email, password } = req.body;
       const userData = await userService.registation(email, password);
       res.cookie('refreshToken', userData.refreshToken, {
@@ -17,12 +25,23 @@ class UserController {
 
   async login(req, res, next) {
     try {
+      const { email, password } = req.body;
+      const userData = await userService.login(email, password);
+      res.cookie('refreshToken', userData.refreshToken, {
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+      });
+      return res.json(userData);
     } catch (e) {
       next(e);
     }
   }
   async logout(req, res, next) {
     try {
+      const { refreshToken } = req.cookies;
+      const token = await userService.logout(refreshToken);
+      res.clearCookie('refreshToken');
+      return res.json(token);
     } catch (e) {
       next(e);
     }
