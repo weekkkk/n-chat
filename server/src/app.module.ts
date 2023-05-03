e871eb6a985d1a458943993ae0dd2e1218153c88
weekkkk <nikitanedelko9@gmail.com>
2023-05-03 15:56:00 +0300

    server upd

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env'
+    }),
+    UserModule ],
 })
 export class AppModule {}
